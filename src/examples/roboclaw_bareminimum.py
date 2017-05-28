f43b3f858de004fd0547ad674c605afb36b4bfa4
ahill2013 <ahill2013@my.fit.edu>
2017-05-28 19:29:29 -0400

    Fixed comport name

--- src/examples/roboclaw_bareminimum.py
+++ src/examples/roboclaw_bareminimum.py
@@ -1,8 +1,8 @@
 from roboclaw import Roboclaw
 
-#Windows comport name
+# Windows comport name
 #rc = Roboclaw("COM3",115200)
-#Linux comport name
-rc = Roboclaw("/dev/roboclaw",115200)
+# Linux comport name
+rc = Roboclaw("/dev/roboclaw", 115200)
 
 rc.Open()
--- src/examples/roboclaw_mixedpwm.py
+++ src/examples/roboclaw_mixedpwm.py
@@ -1,10 +1,10 @@
 import time
 from roboclaw import Roboclaw
 
-#Windows comport name
-rc = Roboclaw("COM9",115200)
-#Linux comport name
-#rc = Roboclaw("/dev/ttyACM0",115200)
+# Windows comport name
+#rc = Roboclaw("COM9",115200)
+# Linux comport name
+rc = Roboclaw("/dev/roboclaw", 115200)
 
 rc.Open()
 address = 0x80
@@ -12,7 +12,7 @@ address = 0x80
 rc.ForwardMixed(address, 0)
 rc.TurnRightMixed(address, 0)
 
-while(1):
+while 1:
 	rc.ForwardMixed(address, 32)
 	time.sleep(2)
 	rc.BackwardMixed(address, 32)
