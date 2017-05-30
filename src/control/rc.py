87c7dd7e722412e761edd1bb19ab594cd12205d0
ahill2013 <ahill2013@my.fit.edu>
2017-05-30 19:18:28 -0400

    Found an example script, read_PWM.py that does exactly what we want. Update rc.py to use its functions.

--- src/control/rc.py
+++ src/control/rc.py
@@ -8,6 +8,7 @@ from amqp.amqplib import Protected
 from time import sleep
 from time import time
 import pigpio
+import read_PWM
 
 
 class RemoteControl(Thread):
@@ -19,10 +20,10 @@ class RemoteControl(Thread):
 
     MILLISECOND = 0.001
 
-    PIN_SWITCH = 8
+    PIN_SWITCH = 14  # 8
     # PIN_ESTOP = 10
-    PIN_LIN = 12
-    PIN_ANG = 16
+    PIN_LIN = 18  # 12
+    PIN_ANG = 23  # 16
     DEADZONE = .05
     RCRANGE = 400
     switch_threshold = 1700
@@ -30,16 +31,11 @@ class RemoteControl(Thread):
     # Initialize GPIO
     # GPIO.setmode(GPIO.BOARD)
     pi = pigpio.pi()
+
     # Set pin 7 to input
-    pi.set_mode(PIN_SWITCH, pigpio.INPUT)
-    pi.set_mode(PIN_LIN, pigpio.INPUT)
-    pi.set_mode(PIN_ANG, pigpio.INPUT)
-    pi.set_pull_up_down(PIN_SWITCH, pigpio.PUD_DOWN)
-    pi.set_pull_up_down(PIN_LIN, pigpio.PUD_DOWN)
-    pi.set_pull_up_down(PIN_ANG, pigpio.PUD_DOWN)
-    sw = pi.callback(PIN_SWITCH, pigpio.EITHER_EDGE)
-    lin = pi.callback(PIN_LIN, pigpio.EITHER_EDGE)
-    ang = pi.callback(PIN_ANG, pigpio.EITHER_EDGE)
+    sw = read_PWM.reader(pi, PIN_SWITCH)
+    lin = read_PWM.reader(pi, PIN_LIN)
+    ang = read_PWM.reader(pi, PIN_ANG)
 
     # GPIO.setup(PIN_SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     # GPIO.setup(PIN_LIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -62,21 +58,18 @@ class RemoteControl(Thread):
 
     def run(self):
         print "Entered run in rc"
-        for x in range(0, 10):
-            self.get_pwm(RemoteControl.lin)
-            self.get_pwm(RemoteControl.ang)
-
-        cent_lin = self.get_pwm(self.lin)
-        cent_ang = self.get_pwm(self.ang)
+        sleep(.5)
+        cent_lin = RemoteControl.lin.pulse_width()
+        cent_ang = RemoteControl.ang.pulse_width()
 
         while self.running.get_value():
             # set mode to False if rc
             # set value to {linvel: value, angvel: value, duration: milliseconds}
-            chlin = self.get_pwm(RemoteControl.lin)
+            chlin = RemoteControl.lin.pulse_width()
             print "chlin: %f" % chlin
-            chang = self.get_pwm(RemoteControl.ang)
+            chang = RemoteControl.ang.pulse_width()
             print "chang: %f" % chang
-            switch = self.get_pwm(RemoteControl.sw)
+            switch = RemoteControl.sw.pulse_width()
             print "switch: %f" % switch
             if switch > RemoteControl.switch_threshold:
                 self.mode.set_value(False)
@@ -97,17 +90,6 @@ class RemoteControl(Thread):
 
     def stop(self):
         self.running.set_value(False)
-        GPIO.cleanup()
-
-    @staticmethod
-    def current_nano_time():
-        return int(round(time()*1000000))
-
-    def get_pwm(self, pin):
-        while pin.level != 1:
-            start = pin.tick
-        while pin.level != 0:
-            end = pin.tick
-        return end - start
+        # GPIO.cleanup()
 
 
